7ff60d7f1df39e0649aae6524714954802af13ed
Syedsameer <syedmahammadsameer999@gmail.com>
2022-05-21 10:42:08 +0530

    Added: rest controller to show message

--- src/main/java/Controller/HelloRestController.java
+++ src/main/java/Controller/HelloRestController.java
@@ -2,6 +2,7 @@ package Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,9 @@ public class HelloRestController {
     @GetMapping("/message")
     public String message() {
         return "Hello From BridgeLabz";
+    }
+    @GetMapping("/mr")
+    public String SayHello(@RequestParam String name) {
+        return "Hello"+name+"Form BridgeLabz";
     }
 }
